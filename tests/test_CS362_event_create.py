ebef1e31e2243716b1108ac8b9f108953204def5
Evan Hopper-Moore <evanhoppermoore@gmail.com>
2019-05-09 22:22:32 -0700

    Assignment 2 tests

---
+++ tests/test_CS362_event_create.py
@@ -0,0 +1,36 @@
+import unittest2
+
+from google.appengine.ext import ndb
+from google.appengine.ext import testbed
+
+from helpers.event.event_test_creator import EventTestCreator
+
+from models.team import Team
+
+
+class TestEventTeamCreator(unittest2.TestCase):
+    def setUp(self):
+        self.testbed = testbed.Testbed()
+        self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
+        ndb.get_context().clear_cache()
+
+        self.testbed.init_taskqueue_stub(root_path=".")
+
+        for team_number in range(7):
+            Team(id="frc%s" % team_number,
+                 team_number=team_number).put()
+
+        self.events = []
+
+    def tearDown(self):
+        for event in self.events:
+            event.key.delete()
+
+        self.testbed.deactivate()
+
+    def test_creates(self):
+        self.events.append(EventTestCreator.createPastEvent())
+        self.events.append(EventTestCreator.createFutureEvent())
+        self.events.append(EventTestCreator.createPresentEvent())
--- tests/test_CS362_match_create.py
+++ tests/test_CS362_match_create.py
@@ -9,6 +9,8 @@ class TestMatch(unittest2.TestCase):
     def setUp(self):
         self.testbed = testbed.Testbed()
         self.testbed.activate()
+        self.testbed.init_datastore_v3_stub()
+        self.testbed.init_memcache_stub()
 
     def tearDown(self):
         self.testbed.deactivate()
@@ -20,6 +22,9 @@ class TestMatch(unittest2.TestCase):
             '1m02s': '62',
             '10s': '10',
             '2m': '120',
+            '12345': '12345',
+            '5h': '18000',
+            '1h2m3s': '3723',
         }
         for old_ts, seconds in data.items():
             match = Match(
@@ -35,7 +40,5 @@ class TestMatch(unittest2.TestCase):
             self.assertListEqual(match.youtube_videos_formatted, ['TqY324xLU4s?start=' + seconds])
 
         # Test that nothing is changed if there is no timestamp
-        match = Match(
-            youtube_videos=['TqY324xLU4s']
-        )
+        match = Match(youtube_videos=['TqY324xLU4s'])
         self.assertListEqual(match.youtube_videos_formatted, ['TqY324xLU4s'])
